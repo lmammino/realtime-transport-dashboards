6e7497fa74fe1b899da68cac01b7fb98f9b20cc8
Luciano <lucianomammino@gmail.com>
2019-09-28 16:54:42 +0100

    Added collection of realtime info

---
+++ src/handlers/getDashboard/getWidgetData.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const dublinBus = require('public-transport-ireland/dublin-bus')
+const irishRail = require('public-transport-ireland/irish-rail')
+const luas = require('public-transport-ireland/luas')
+
+async function getWidgetData ({ type, parameters }) {
+  if (type === 'dublinBus') {
+    return getDublinBusData(parameters)
+  }
+
+  if (type === 'irishRail') {
+    return getIrishRailData(parameters)
+  }
+
+  if (type === 'luas') {
+    return getLuasData(parameters)
+  }
+
+  throw new Error(`Invalid widget type. Found "${type}", expected "dublinBus", "irishRail" or "luas"`)
+}
+
+async function getDublinBusData (params) {
+  return dublinBus.getRealTimeInfo(params.id)
+}
+
+async function getIrishRailData (params) {
+  return irishRail.getRealTimeInfo(params.code, params.direction)
+}
+
+async function getLuasData (params) {
+  return luas.getRealTimeInfo(params.code, params.direction)
+}
+
+module.exports = getWidgetData
---
+++ src/handlers/getDashboard/index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const getWidgetData = require('./getWidgetData')
+
+module.exports = function factory (dynamoClient, tableName) {
+  return async function getWidget (event) {
+    const dashboardId = event.pathParameters.dashboard_id
+
+    const { Item: dashboard } = await dynamoClient.get({
+      TableName: tableName,
+      Key: {
+        id: dashboardId
+      }
+    }).promise()
+
+    const expandedWidget = await Promise.all(
+      dashboard.widgets.map(async (widget) => {
+        const realtimeInfo = await getWidgetData(widget.config)
+        return Object.assign({}, widget, { realtimeInfo })
+      })
+    )
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(Object.assign({}, dashboard, { widgets: expandedWidget }))
+    }
+  }
+}
